2267c5305694725b6ceaf59d94937b5391148ccd
Ferdie De Oliveira <code@bigyell.co.uk>
2017-01-27 16:54:44 +0000

    Mass assignment

--- src/Model/EventModel.php
+++ src/Model/EventModel.php
@@ -4,8 +4,17 @@ namespace deferdie\Event\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
-
 class EventModel extends Model
 {
-  protected $table = 'events';
+  	protected $table = 'events';
+
+  	protected $fillable = [
+    	'user_id',
+		'title',
+		'date',
+		'image',
+		'description',
+		'canBook',
+		'active'
+    ];
 }
